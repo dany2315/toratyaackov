6f19911396ea29d937adcd244e49798301d48021
dany2315 <davidserfaty2315@gmail.com>
2024-06-25 19:44:29 +0300

    azerty

--- serveur/server.js
+++ serveur/server.js
@@ -11,11 +11,10 @@ import { Storage } from "@google-cloud/storage";
 
 
 const ENV = process.env.ENV;
-const URL_DATABASE_SCALINGO = process.env.SCALINGO_MONGO_URL;
-const URL_DATABASE_DEV = process.env.URL_DATABASE;  //URL_DATABASE
+// const URL_DATABASE_SCALINGO = process.env.SCALINGO_MONGO_URL;
+// const URL_DATABASE_DEV = process.env.URL_DATABASE;  //URL_DATABASE
 const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 console.log(path.join(__dirname, "../build"));
 console.log(ENV);
@@ -24,14 +23,14 @@ console.log(ENV);
 const app = express(); 
 
 
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(ENV === "dev" ? URL_DATABASE_DEV : URL_DATABASE_SCALINGO)
-  .then(() =>{
-    console.log(`connect to mongoDB: ${ENV}`)})
-  .catch((error) =>
-    console.log("probleme de connexion a mongoDB " + error.message)
-  );
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(ENV === "dev" ? URL_DATABASE_DEV : URL_DATABASE_SCALINGO)
+//   .then(() =>{
+//     console.log(`connect to mongoDB: ${ENV}`)})
+//   .catch((error) =>
+//     console.log("probleme de connexion a mongoDB " + error.message)
+//   );
 
 
   
@@ -58,10 +57,6 @@ mongoose
     res.send("welcome to the home page");
   });
   
-
-app.get('/', (req, res) => {
-  res.send('Hello Wd!')
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port  ${PORT}`)
