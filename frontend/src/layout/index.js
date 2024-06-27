deb32334df2c2e5f11ad1e410a3420ba5b509c83
dany2315 <davidserfaty2315@gmail.com>
2024-06-27 19:25:04 +0300

    safe area

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -13,7 +13,7 @@ function Layout() {
 
   return (
     <>
-      <div className="flex flex-col overflow-hidden h-screen  w-[100%]   bg-[#F3F3F3]  relative">
+      <div className="flex flex-col overflow-hidden safe-area h-screen  w-[100%]   bg-[#F3F3F3]  relative">
         <div className=" bg-[#F3F3F3] shadow-2xl sticky top-0 w-full z-50">
           <Navbar/>
         </div>
